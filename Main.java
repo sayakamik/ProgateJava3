371eb377918b6e072b25df2a0cc4f1ae3961a179
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-12-05 12:01:30 +0000

    メソッド2-4 真偽値の戻り値

--- Main.java
+++ Main.java
@@ -9,26 +9,33 @@ public class Main {
     System.out.println("年齢は" + age + "歳です");
     System.out.println("身長は" + height + "mです");
     System.out.println("体重は" + weight + "kgです");
-    
-    // 変数bmiに、bmiメソッドを用いてBMIを代入してください
     double bmi = bmi(height, weight);
-    
-    // 変数bmiを用いて、「BMIは◯◯です」と出力してください
     System.out.println("BMIは" + bmi + "です");
+    
+    // isHealthyメソッドの結果で条件分岐を行ってください
+    if (isHealthy(bmi)) {
+      System.out.println("健康です");
+    } else {
+      System.out.println("健康ではありません");
+    }
     
   }
 
   public static String fullName(String firstName, String lastName) {
     return firstName + " " + lastName;
   }
-  
+
   public static String fullName(String firstName, String middleName, String lastName) {
     return firstName + " " + middleName + " " + lastName;
   }
-  
-  // bmiメソッドを定義してください
+
   public static double bmi(double height, double weight) {
     return weight / height / height;
+  }
+
+  // isHealthyメソッドを定義してください
+  public static boolean isHealthy(double bmi) {
+    return bmi >= 18.5 && bmi < 25.0;
   }
   
 }
